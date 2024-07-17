112c4f0559737afbd2fcd0a805ab4d56a8bd6c55
Not_H3 <67106546+damger9@users.noreply.github.com>
2024-07-17 21:03:40 +0200

    Create TimestampVariant.cs

---
+++ Disutility/Timestamps/TimestampVariant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordUtilities.Timestamps {
+    public enum ETimestampVariant {
+        // TODO -> Add explanations!
+        ShortTime = 116,
+        LongTime = 84,
+        ShortDate = 100,
+        LongDate = 68,
+        ShortDateTime = 102,
+        LongDateTime = 70,
+    }
+}
