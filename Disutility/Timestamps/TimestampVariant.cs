8255f494e8ca97af368c7e23051f0ff093ea32f1
Not_H3 <67106546+damger9@users.noreply.github.com>
2024-07-17 21:18:57 +0200

    Explanations for timestamp Variants

--- Disutility/Timestamps/TimestampVariant.cs
+++ Disutility/Timestamps/TimestampVariant.cs
@@ -6,12 +6,29 @@ using System.Threading.Tasks;
 
 namespace DiscordUtilities.Timestamps {
     public enum ETimestampVariant {
-        // TODO -> Add explanations!
+        /// <summary>
+        /// Example: "10:12AM" 
+        /// </summary>
         ShortTime = 116,
+        /// <summary>
+        /// Example: "10:12:00AM" 
+        /// </summary>
         LongTime = 84,
+        /// <summary>
+        /// Example: "7/17/24" or "17/7/24" (Depends on the user that sees the timestamp, Discord localizes it. 
+        /// </summary>
         ShortDate = 100,
+        /// <summary>
+        /// Example: "July 17, 2024" 
+        /// </summary>
         LongDate = 68,
+        /// <summary>
+        /// Example: "July 17, 2024 at 10:12AM" 
+        /// </summary>
         ShortDateTime = 102,
+        /// <summary>
+        /// Example: "Wednesday, July 17, 2024 at 10:12AM" 
+        /// </summary>
         LongDateTime = 70,
     }
 }
