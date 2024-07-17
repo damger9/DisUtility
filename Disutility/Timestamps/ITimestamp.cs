1c7912c34f90c859b2aedf436516ffacd741905c
Not_H3 <67106546+damger9@users.noreply.github.com>
2024-07-17 21:02:57 +0200

    Create ITimestamp.cs

---
+++ Disutility/Timestamps/ITimestamp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordUtilities.Timestamps {
+    public interface ITimestamp {
+        string GetRelative(DateTimeOffset timestamp);
+        string GetRelative(DateTime timestamp);
+        string GetRelative();
+        string GetAbsolute(DateTime timestamp, ETimestampVariant timestampVariant);
+    }
+}
